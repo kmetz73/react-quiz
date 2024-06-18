ffc847a6da8e67e3bbb21f71e5dba1cff28223e9
kmetz73 <kmetz73@gmail.com>
2024-06-18 13:09:15 -0500

    Added  useReducer to  date counter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import DateCounter from "./components/DateCounter";
+
 export default function App() {
   return (
     <div>
-      <h1>Hello World</h1>
-      <p>This is my first react app</p>
+      <DateCounter />
     </div>
   );
 }
